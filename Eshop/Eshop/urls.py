dafd1b934ce75421b245ef1c2e860b09774a04c8
Laya Augustine <layaaugustine12345@gmail.com>
2022-06-05 11:55:42 +0530

    modified

--- Eshop/Eshop/urls.py
+++ Eshop/Eshop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
@@ -21,5 +23,5 @@ from store import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(store.urls))
-]
+    path('',include(store.urls)),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
