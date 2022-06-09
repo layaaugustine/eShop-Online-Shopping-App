43036ce14bfa1a2b8a47a089e33776df09fe27ca
Laya Augustine <layaaugustine12345@gmail.com>
2022-06-09 21:10:59 +0530

    modified

--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -4,14 +4,20 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from numpy import product
-from .models import Product
+from .models import Product,Category
 
 # Create your views here.
 
+# def index(request):
+#     datasample = loader.get_template('index.html')
+#     print(Product.objects.all())
+#     prds=Product.objects.all
+#     data={'products':prds}
+#     response=datasample.render(data,request)
+#     return HttpResponse(response)
+
 def index(request):
-    datasample = loader.get_template('index.html')
-    print(Product.objects.all())
-    prds=Product.objects.all
-    data={'products':prds}
-    response=datasample.render(data,request)
-    return HttpResponse(response)
+    prdts = Product.objects.all()
+    catgry=Category.objects.all()
+    data={'products':prdts,'category':catgry}
+    return render(request,'index.html',data)
