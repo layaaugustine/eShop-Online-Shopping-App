db463ce8d54e2da11dfa0a78ef8c750011b723c4
Laya Augustine <layaaugustine12345@gmail.com>
2022-06-09 20:02:20 +0530

    modified

--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -9,7 +9,7 @@ from .models import Product
 # Create your views here.
 
 def index(request):
-    datasample = loader.get_template('index.html')
+    datasample = loader.get_template('Order/order.html')
     print(Product.objects.all())
     prds=Product.objects.all
     data={'products':prds}
