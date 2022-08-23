ff3528de7de38ec77314bdeb7670b7e6bd6b3bec
Laya Augustine <layaaugustine12345@gmail.com>
2022-08-23 16:55:12 +0530

    add products

--- Eshop/store/admin.py
+++ Eshop/store/admin.py
@@ -1,8 +1,9 @@
-from .models import Customer,Category,Product,Order,AdminProduct,AdminCategory
+from .models import Customer,Category,Product,Order,AdminProduct,AdminCategory,AdminCustomer
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(Product,AdminProduct)
 admin.site.register(Category,AdminCategory)
+admin.site.register(Customer,AdminCustomer)
 admin.site.register(Order)
